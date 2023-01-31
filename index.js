85062f8b58fca968baa9520989f77e615a3aabc9
Tiffany Mason <tiffanyaamason@gmail.com>
2023-01-31 14:45:29 -0600

    Add menu button that opens and closes added js function to open and close a menu on click

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const nav = document.getElementById("topNav");
-window.onScroll = function () {
-  if (window.pageYOffset > 100) {
-    nav.classList.add("sticky");
-  } else {
-    nav.classList.remove("sticky");
-  }
-}
+// const nav = document.getElementById("topNav");
+// window.onScroll = function () {
+//   if (window.pageYOffset > 100) {
+//     nav.classList.add("sticky");
+//   } else {
+//     nav.classList.remove("sticky");
+//   }
+// }
+
+
+
+
+const navList = document.querySelector('.nav-list');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+ const visibility = navList.getAttribute('data-visible');
+
+ if (visibility === "false") {
+  navList.setAttribute('data-visible', true);
+  navToggle.setAttribute('aria-expanded', true);
+ } else if (visibility === 'true') {
+  navList.setAttribute('data-visible', false);
+  navToggle.setAttribute('aria-expanded', false);
+ }
+});
+
+
+
+
+
