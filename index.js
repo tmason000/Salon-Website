8ab8ce6f888601329cba0b7bdb7d858014ceb764
Tiffany Mason <tiffanyaamason@gmail.com>
2023-02-02 08:22:40 -0600

    Add pop-up box added a popup box with new client offer

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const popup = document.querySelector('.popup');
+const close = document.querySelector('.close');
+
+window.onload = function() {
+  setTimeout(function(){
+    popup.style.display = "block";
+  }, 2000)
+}
+
+close.addEventListener('click', () => {
+  popup.style.display = "none";
+})
+
 const nav = document.getElementById("topNav");
 window.onscroll = function () {
   if (window.pageYOffset > 600) {
